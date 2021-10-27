22feb84b4f40f92c9c2ef02552e1d199c1d34dc9
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-10-27 14:31:37 +0530

    Added: Media compatibitlity

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/style.css";
+import accounts from "../assets/accounts.PNG"
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -17,102 +18,109 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 export class Registration extends React.Component {
   render() {
     return (
-      <div className="outerBox">
-        <div className="outerPadding">
-          <div>
-            <span className="fundooNotesRainbow">FundooNotes</span>
-          </div>
-          <div className="createAccountDiv">
-            <span className="createAccount">Create your Note Account</span>
-          </div>
-          <form>
-            <div className="inputBox">
-              <div className="firstAndLast">
-                <div className="firstName">
-                  <TextField
-                    required
-                    className="firstNameBox"
-                    label="First name"
-                    variant="outlined"
-                    size="small"
-                  />
-                </div>
-                <div className="lastName">
-                  <TextField
-                    required
-                    className="lastNameBox"
-                    label="Last name"
-                    variant="outlined"
-                    size="small"
-                  />
-                </div>
-              </div>
-              <div className="emailId">
-                <TextField
-                  helperText="You can enter letters, numbers and periods"
-                  required
-                  className="emailIdBox"
-                  fullWidth
-                  label="Email Id"
-                  size="small"
-                  autoComplete="email"
-                  placeholder="abc.123@example.com"
-                  //   InputProps={{endAdornment: <InputAdornment position="end">@example.com</InputAdornment>}}
-                />
-              </div>
-              <div className="password">
-                <div className="firstPassword">
-                  <TextField
-                    type="password"
-                    required
-                    className="firstPasswordBox"
-                    label="Password"
-                    variant="outlined"
-                    size="small"
-                  />
-                </div>
-                <div className="confirm">
-                  <TextField
-                    type="password"
-                    required
-                    className="confirmBox"
-                    label="Confirm"
-                    variant="outlined"
-                    size="small"
-                  />
-                </div>
-              </div>
-              <div className="passwordInfo">
-                <span>
-                  Use 8 or more characters with a mix of letters, numbers &
-                  symbols
-                </span>
-              </div>
-              <div className="showPasswordMsg">
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      value="allowExtraEmails"
-                      color="primary" /*onClick={myFunction}*/
-                    />
-                  }
-                  label={
-                    <Typography style={{ fontSize: "14px" }}>
-                      Show password
-                    </Typography>
-                  }
-                />
-                </div>
-              <div className="signInSignUp">
-              <div className="signIn">
-                <span>Sign in instead</span>
-              </div>
-              <div className="signUp">
-                <Button variant="contained">Sign Up</Button>
-              </div>
-              </div>
+      <div className="imgBox">
+        <div className="outerBox">
+          <div className="outerPadding">
+            <div>
+              <span className="fundooNotesRainbow">FundooNotes</span>
             </div>
-          </form>
+            <div className="createAccountDiv">
+              <span className="createAccount">Create your Note Account</span>
+            </div>
+            <form>
+              <div className="innerImg">
+                <div className="inputBox">
+                  <div className="firstAndLast">
+                    <div className="firstName">
+                      <TextField
+                        required
+                        className="firstNameBox"
+                        label="First name"
+                        variant="outlined"
+                        size="small"
+                      />
+                    </div>
+                    <div className="lastName">
+                      <TextField
+                        required
+                        className="lastNameBox"
+                        label="Last name"
+                        variant="outlined"
+                        size="small"
+                      />
+                    </div>
+                  </div>
+                  <div className="emailId">
+                    <TextField
+                      helperText="You can enter letters, numbers and periods"
+                      required
+                      className="emailIdBox"
+                      fullWidth
+                      label="Email Id"
+                      size="small"
+                      autoComplete="email"
+                      placeholder="abc.123@example.com"
+                      //   InputProps={{endAdornment: <InputAdornment position="end">@example.com</InputAdornment>}}
+                    />
+                  </div>
+                  <div className="password">
+                    <div className="firstPassword">
+                      <TextField
+                        type="password"
+                        required
+                        className="firstPasswordBox"
+                        label="Password"
+                        variant="outlined"
+                        size="small"
+                      />
+                    </div>
+                    <div className="confirm">
+                      <TextField
+                        type="password"
+                        required
+                        className="confirmBox"
+                        label="Confirm"
+                        variant="outlined"
+                        size="small"
+                      />
+                    </div>
+                  </div>
+                  <div className="passwordInfo">
+                    <span>
+                      Use 8 or more characters with a mix of letters, numbers &
+                      symbols
+                    </span>
+                  </div>
+                  <div className="showPasswordMsg">
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          value="allowExtraEmails"
+                          color="primary" /*onClick={myFunction(confirm)}*/
+                        />
+                      }
+                      label={
+                        <Typography style={{ fontSize: "14px" }}>
+                          Show password
+                        </Typography>
+                      }
+                    />
+                  </div>
+                  <div className="signInSignUp">
+                    <div className="signIn">
+                      <span>Sign in instead</span>
+                    </div>
+                    <div className="signUp">
+                      <Button variant="contained">Sign Up</Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="img">
+                      <img src={accounts} width={260} height={244} style={{verticalAlign: 'middle'}}/>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     );
